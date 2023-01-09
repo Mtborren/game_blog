790c9163c9c33657a670a9fa3d913852c8a73e7a
mtborren <mtborren@gmail.com>
2023-01-09 14:48:26 -0500

    basic django api functionality added

--- game_blog/game_blog/admin.py
+++ game_blog/game_blog/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Game
+from .models import Game, Platform, Genre
 
 admin.site.register(Game)
+admin.site.register(Platform)
+admin.site.register(Genre)
